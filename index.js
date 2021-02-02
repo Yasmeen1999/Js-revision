7e72df14f05a1bf894121ba596d7eb46007e1a2e
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2021-02-03 01:38:46 +0530

    Js revision

---
+++ index.js
@@ -0,0 +1,78 @@
+
+                        /*   statement  */
+
+// const temparatureToday = 86;
+
+// if (temparatureToday === 86){
+//     console.log("cool")
+// }else {
+//  console.log("yahh!!!")
+// }
+
+
+                           /*  for loop  */
+
+// const character = "a";
+// const times = 50;
+// let answer ="";
+
+// for (i=0; i<= times; i++){
+//  answer += character;
+// }
+// console.log(answer.length);
+
+
+                          /*   function  declaration  */
+
+
+// function addTwo(number){
+//     return number +2 ;
+// }
+
+// const finalAnswer = addTwo(4);
+// console.log(finalAnswer);
+
+
+
+                          /*  arrow function    */
+
+
+//  addTwo = number =>{ return number +2 ;}
+
+// const finalAnswer = addTwo(4);
+// console.log(finalAnswer);
+
+
+            /* object*/ 
+
+//  const person = {
+//      name:"yasmeen",
+//      place:"bangaloru",
+//      age: 21,
+//  }          
+//  console.log(person.place) 
+
+
+
+ /*
+ <style>
+  .color-box {
+    background-color: limegreen;
+    width: 100px;
+    height: 100px;
+  }
+</style>
+<div class="color-box"></div>
+<input class="color-input" placeholder="Type a color here!" />
+<script>
+  const input = document.querySelector('.color-input');
+  const paragraph = document.querySelector('.color-box');
+
+  input.addEventListener("change", function() {
+    paragraph.style.backgroundColor  = input.value;
+  });
+</script>
+ 
+ */
+
+
